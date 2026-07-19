f76b7e4c7f8ee80f8a3930dd129190c7e46e3a67
tunaflsh <tunaflish.ai@gmail.com>
2026-07-20 01:12:42 +0200

    Initial commit

---
+++ src/client/java/tunaflsh/wynnveil/client/WynnVeilClient.java
@@ -0,0 +1,10 @@
+package tunaflsh.wynnveil.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WynnVeilClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
